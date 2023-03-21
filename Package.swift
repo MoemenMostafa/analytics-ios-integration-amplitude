b05899dd5f360cb7dcea4bffb97a262e669379a4
Moamen Mostafa <moemen.a1@gmail.com>
2023-03-21 14:02:04 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
 		.library(name: "Segment_Amplitude", targets: ["Segment_Amplitude"]),
 	],
 	dependencies: [
-		.package(name: "Amplitude", url: "https://github.com/amplitude/Amplitude-iOS", .upToNextMinor(from: "8.3.1")),
+		.package(name: "Amplitude", url: "https://github.com/amplitude/Amplitude-iOS", .upToNextMinor(from: "8.15.2")),
 		.package(name: "Segment", url: "https://github.com/segmentio/analytics-ios", .upToNextMinor(from: "4.1.4"))
 	],
 	targets: [
